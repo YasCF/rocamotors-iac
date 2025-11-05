bc6a6e6c436712316590f65f61446655b1f85358
Yas-CF <yasna.kcf@gmail.com>
2025-11-04 23:09:33 -0300

    segundo workflows

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -4,6 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "roca-bucket-aws"
+    key            = "terraform/rocamotors-iac.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "tf-locks"
   }
   backend "s3" {
     bucket         = "roca-bucket-aws"
@@ -17,16 +29,9 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  host                   = try(data.aws_eks_cluster.cluster.endpoint, "")
+  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data), "")
+  token                  = try(data.aws_eks_cluster_auth.cluster.token, "")
 }
